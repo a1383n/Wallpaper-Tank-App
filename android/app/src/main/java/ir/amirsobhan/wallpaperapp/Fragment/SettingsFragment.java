89f54498ca55c4877100db09ff30adec1e859fc1
Amirsobhan Nafariyeh <amirsobhan1553@gmail.com>
2020-12-26 23:47:39 +0330

    Dark mode added
    Add dark mode theme
    Add customCheckBoxPreference

--- android/app/src/main/java/ir/amirsobhan/wallpaperapp/Fragment/SettingsFragment.java
+++ android/app/src/main/java/ir/amirsobhan/wallpaperapp/Fragment/SettingsFragment.java
@@ -1,15 +1,43 @@
 package ir.amirsobhan.wallpaperapp.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import ir.amirsobhan.wallpaperapp.MainActivity;
 import ir.amirsobhan.wallpaperapp.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+    SwitchPreferenceCompat switchDark;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+
+        switchDark = findPreference("dark");
+        switchDark.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                Snackbar.make(getView(), "Saved,The app must be restarted to apply the new theme", BaseTransientBottomBar.LENGTH_INDEFINITE)
+                        .setAnchorView(getActivity().findViewById(R.id.bottom_navigation))
+                        .setAction("Restart", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startActivity(new Intent(getActivity(), MainActivity.class));
+                                getActivity().finish();
+                            }
+                        })
+                        .show();
+                return true;
+            }
+        });
     }
 }
--- android/app/src/main/java/ir/amirsobhan/wallpaperapp/MainActivity.java
+++ android/app/src/main/java/ir/amirsobhan/wallpaperapp/MainActivity.java
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -26,10 +30,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences.getBoolean("dark",false)){
+            setTheme(R.style.DarkTheme);
+        }else{
+            setTheme(R.style.AppTheme);
+        }
+
         setContentView(R.layout.activity_main);
-
-
-
         // Initialization Views
         Initialization();
 
