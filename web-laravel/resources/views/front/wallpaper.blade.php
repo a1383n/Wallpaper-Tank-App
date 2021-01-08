0ea2f219e753f08957b44d26b317a391a85d9d7d
Amirsobhan Nafariyeh <amirsobhan1553@gmail.com>
2021-01-08 21:50:14 +0330

    Show wallpaper complete
    wallpaper.blade.php almost complete

--- web-laravel/resources/views/front/wallpaper.blade.php
+++ web-laravel/resources/views/front/wallpaper.blade.php
@@ -9,37 +9,53 @@
     <title>{{$wallpaper->title}}</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
     <style>
-        body{
+        body {
             background: #333;
             margin: 0;
             padding: 0;
             border: 0;
-            font-family: 'Open Sans',sans-serif;
+            font-family: 'Open Sans', sans-serif;
+            overflow: hidden;
         }
+
         .box {
             background: #fff;
             border-radius: 2px;
-            height: 500px;
+            height: auto;
             margin: 1rem;
             box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
         }
 
-        .box-image{
+        .box-image {
             width: 30%;
-            height: 500px;
+            height: auto;
             display: inline-block;
         }
 
-        .box-image img{
+        .box-image img {
             width: 100%;
             height: 500px;
         }
-        .box-body{
+
+        .box-body {
             width: 70%;
-            height: 500px;
+            height: auto;
             display: inline-block;
             float: right;
             padding: 15px;
+        }
+
+        .box-info {
+            height: 370px;
+        }
+
+        .tags {
+            margin-right: 10px;
+            margin-bottom: 10px;
+        }
+
+        .box-footer {
+
         }
     </style>
 </head>
@@ -51,13 +67,39 @@
         </div>
         <div class="box-body">
             <div class="box-title">
-                <h2 class="h2">
+                <h1 class="h1 d-inline-block">
                     {{$wallpaper->title}}
-                </h2>
+                </h1>
+                <button class="btn btn-danger d-inline-block float-right m-2">Like&nbsp;<li class="fa fa-heart"></li>
+                </button>
+                <button class="btn btn-primary d-inline-block float-right m-2">Download&nbsp;<li
+                        class="fa fa-download"></li>
+                </button>
             </div>
             <div class="box-info">
-                @inject('categoryController','\App\Http\Controllers\CategoryController')
-                <h4>Category: <a href="#">{{$categoryController->show(new \App\Models\Category,$wallpaper->category_id)->title}}</a></h4>
+                <h4>Category: <a href="#">{{\App\Models\Category::find($wallpaper->category_id)->title}}</a></h4>
+                <h4>Author: {{\App\Models\User::find($wallpaper->user_id)->name}}</h4>
+                <h4>Tags:</h4>
+                <div class="tags-list">
+                    @foreach(explode(",",$wallpaper->tags) as $tag)
+                        <button class="btn btn-secondary tags">{{$tag}}</button>
+                    @endforeach
+                </div>
+            </div>
+            <div class="box-footer">
+                <div class="d-inline-block">
+                    <span class="badge">
+                    Create at: {{$wallpaper->created_at}}
+                    </span>
+                    <br>
+                    <span class="badge">
+                    Last update at: {{$wallpaper->updated_at}}
+                    </span>
+                </div>
+                <div class="d-inline-block float-right">
+                    <br>
+                    <p class="badge badge-dark">{{$wallpaper->views}}&nbsp;Views</p>
+                </div>
             </div>
         </div>
     </div>
