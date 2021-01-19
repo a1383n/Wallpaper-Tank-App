5e14be06218c5ebd2a4dcb3f0c218317f3a31fbf
Amirsobhan Nafariyeh <amirsobhan1553@gmail.com>
2021-01-19 18:25:56 +0330

    Improved file names

--- web/api/web/action/authentication.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!isset($_SERVER['HTTP_AUTHENTICATION']) || @$_SERVER['HTTP_AUTHENTICATION'] != API_KEY){
-    http_response_code(401);
-    exit();
-}
--- web/api/web/action/getCategory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require "authentication.php";
-
-if (isset($_POST['id'])) {
-    echo json_encode($category->Select($_POST['id']));
-}else{
-    echo json_encode($category->Select());
-}
